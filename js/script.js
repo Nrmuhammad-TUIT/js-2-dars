92ed9db28a40b9f3b244bca32e5d64f847e54caa
Nurmuhammad-TUIT <nurmuhammad09062020@gmail.com>
2022-02-20 22:11:29 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+alert("Welcome to uzairways!");
+
+var firstName = Number(prompt("Summangizni kiriting (sumda)"));
+
+if (firstName >= 10000000) {
+     console.log("Bosing aka oq yo'l");
+}
+if (firstName < 10000000) {
+   console.log("Aka bu fikringizdan qayting");
+}
+
+alert ("Bizning xizmatdan foydalanganingiz uchun tashakkur");
